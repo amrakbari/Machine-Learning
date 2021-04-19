6f1edb15f2531357130ff9094a4a205898b6796e
amirak <akbariamirreza2001@gmail.com>
2021-04-19 10:04:26 +0430

    KNN done

--- classification/KNN.py
+++ classification/KNN.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
 
@@ -13,3 +14,12 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.fit_transform(x_test)
+
+classifier = KNeighborsClassifier()
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+
